461561340d23ec58f5ea3309325568fc9b182112
TejasriKotha <82953784+TejasriKotha@users.noreply.github.com>
2021-04-23 12:31:05 +0530

    Update WordReverse.java

--- mantra tech/WordReverse.java
+++ mantra tech/WordReverse.java
@@ -3,10 +3,10 @@ package com.tech;
 public class SortNumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		
 		int[] input = new int[] {-5, 6, -7, 3, -1, 3, 9};
-		//int[] input = new int[] {5, 2, 1, 3, 4, 6};
+		
 		int temp=0;
 		
 		for(int i=0; i< input.length; i++) {
