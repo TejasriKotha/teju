85b44f45822b9d800130b5c91d691c8460eeb51b
TejasriKotha <82953784+TejasriKotha@users.noreply.github.com>
2021-04-23 12:34:40 +0530

    Create SortNumbers.java

---
+++ mantra tech/SortNumbers.java
@@ -0,0 +1,30 @@
+package com.tech;
+
+public class SortNumbers {
+
+	public static void main(String[] args) {
+		
+		
+		int[] input = new int[] {-5, 6, -7, 3, -1, 3, 9};
+		
+		int temp=0;
+		
+		for(int i=0; i< input.length; i++) {
+			for(int j=i+1; j< input.length; j++) {
+				if(input[i] > input[j]) {
+					temp=input[i];
+					input[i]=input[j];
+					input[j] = temp;
+				}
+				
+				
+			}
+		}
+		
+		for(int i=0; i<input.length; i++) {
+			System.out.print(input[i]+",");
+		}
+
+	}
+
+}
